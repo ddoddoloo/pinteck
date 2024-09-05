dc9762d834119a68eb154f415dc06ca207376097
JIN <akasagi@naver.com>
2024-09-06 04:20:06 +0900

    first

---
+++ src/main/java/com/example/pinteck/PinteckApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinteck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinteckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinteckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pinteck/PinteckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinteck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinteckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
